6a7dc2a62fedc683ba06d0751cc2df305645bdca
samuel.santiago@Mottu.com.br <samuel.santiago@Mottu.com.br>
2022-05-20 17:41:09 -0300

    teste projeto privado

--- TrialTeste/WinFormsApp1/Form1.Designer.cs
+++ TrialTeste/WinFormsApp1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -53,23 +54,34 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(214, 217);
+            this.button2.Location = new System.Drawing.Point(27, 217);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(320, 189);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(448, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(320, 189);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,5 +92,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
